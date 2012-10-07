f3bff80929cc35be5b08ff64d13ad743697fff7e
Christopher Loessl <cloessl@x-berg.de>
2012-10-07 23:05:47 +0200

    ddg.js build with makebookmarklet.pl
    - added encoding of the search string
    - changed tabs to spaces
    - removed empty lines with only spaces

--- ddg.js
+++ ddg.js
@@ -1,24 +1,18 @@
+// javascript:javascript:%20ddg();function%20ddg()%20{var%20e%20=%20encodeURIComponent;var%20t%20=%20window.getSelection%20?%20window.getSelection()%20:%20(document.getSelection%20?%20document.getSelection()%20:%20(document.selection%20?%20document.selection.createRange().text%20:%20%27%27));if%20(t%20==%20%27%27)%20{var%20defText%20=%20%27!%27;}%20else%20{var%20defText%20=%20t;}var%20c%20=%20window.prompt(%27Search%20on%20DuckDuckGo%27,%20defText);window.location%20=%20%27https://duckduckgo.com/?q=%27%20+%20e(c);}
 javascript: ddg();
 
 function ddg() {
     var e = encodeURIComponent;
     var t = window.getSelection ? window.getSelection() : (document.getSelection ? document.getSelection() : (document.selection ? document.selection.createRange().text : ''));
-    
+
     if (t == '') {
-    	var defText = '!';
+        var defText = '!';
     } else {
-    	var defText = t;
+        var defText = t;
     }
-    
+
     var c = window.prompt('Search on DuckDuckGo', defText);
-    
-    window.location = 'https://duckduckgo.com/?q=' + c;
-    
+
+    window.location = 'https://duckduckgo.com/?q=' + e(c);
+
 }
-
-
-/*
-javascript:ddg();function ddg(){var e=encodeURIComponent;var t=window.getSelection?window.getSelection():(document.getSelection?document.getSelection():(document.selection?document.selection.createRange().text:''));if(t==''){var defText='!';}else{var defText=t;}var c=window.prompt('Search on DuckDuckGo',defText);window.location='https://duckduckgo.com/?q='+c;}
-created with:
-http://www.subsimple.com/bookmarklets/jsbuilder.htm
-*/
